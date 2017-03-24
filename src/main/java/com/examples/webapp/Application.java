75b18a24e2226a4cc3f9be737e6b1a9b84c01b2d
nicole-cranon <nicole.cranon@gmail.com>
2017-03-24 09:16:32 -0600

    initial project commit

---
+++ src/main/java/com/examples/webapp/Application.java
@@ -0,0 +1,15 @@
+package com.examples.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by U999716 on 3/24/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
