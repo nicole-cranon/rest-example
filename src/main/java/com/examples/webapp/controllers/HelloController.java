834f3821c537bf0912950b891b30293b53b01682
nicole-cranon <nicole.cranon@gmail.com>
2017-03-27 10:20:19 -0600

    added simple form support

--- src/main/java/com/examples/webapp/controllers/HelloController.java
+++ src/main/java/com/examples/webapp/controllers/HelloController.java
@@ -1,17 +1,33 @@
 package com.examples.webapp.controllers;
 
+import com.examples.webapp.model.HelloForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by U999716 on 3/24/2017.
  */
 @Controller
 public class HelloController {
-  @RequestMapping("/hello")
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String hello(Model model, @RequestParam(value="name", required=false, defaultValue = "World") String name) {
     model.addAttribute("name", name);
     return "hello";
+  }
+
+  @RequestMapping(value = "/helloform", method = RequestMethod.GET)
+  public ModelAndView helloForm() {
+    return new ModelAndView("helloForm", "command", new HelloForm());
+  }
+
+  @RequestMapping(value = "/helloform", method = RequestMethod.POST)
+  public String helloForm (@ModelAttribute HelloForm helloForm, ModelMap model) {
+    model.addAttribute("firstName", helloForm.firstName);
+    model.addAttribute("lastName", helloForm.lastName);
+
+    return "helloFormResult";
   }
 }
