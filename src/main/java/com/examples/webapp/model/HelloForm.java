dae9edecb08ed9eefc627ad0be0b3480fab88b55
nicole-cranon <nicole.cranon@gmail.com>
2017-03-27 10:20:38 -0600

    HelloForm.java added

---
+++ src/main/java/com/examples/webapp/model/HelloForm.java
@@ -0,0 +1,25 @@
+package com.examples.webapp.model;
+
+/**
+ * Created by U999716 on 3/27/2017.
+ */
+public final class HelloForm {
+  public String firstName;
+  public String lastName;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
